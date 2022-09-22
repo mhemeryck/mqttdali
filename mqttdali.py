a81cb74c403d4500924501f1bb3387fbc0c0590e
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2022-09-22 22:48:23 +0200

    Refactor: 2 separate MQTT functions

--- mqttdali.py
+++ mqttdali.py
@@ -9,8 +9,8 @@ import asyncio_mqtt  # type: ignore
 import dali.address  # type: ignore
 import dali.driver.base  # type: ignore
 import dali.driver.unipi  # type: ignore
-import paho.mqtt.client  # type: ignore
-from dali.gear.general import DAPC, Off  # type: ignore
+from dali.address import Short  # type: ignore
+from dali.gear.general import DAPC, Off, QueryActualLevel  # type: ignore
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -26,64 +26,88 @@ _BRIGHTNESS_OP = "brightness"
 
 _TOPIC_FMT = "{device_name}/lights/{number}/{op}/state"
 _CACHE: typing.Dict[int, int] = {}
+_CACHE_LOCK = asyncio.Lock()
 
 
-async def process_message(
-    message: paho.mqtt.client.MQTTMessage,
+async def light_command_messages(
     driver: dali.driver.base.SyncDALIDriver,
     mqtt_client: asyncio_mqtt.Client,
     device_name: str,
 ) -> None:
-    """Handle single message"""
-    logger.info(f"Incoming message: {message.payload}")
 
-    topic_regex = re.compile(
-        r"{device_name}/lights/(?P<number>\d+)/(?P<op>(status|brightness))/set".format(device_name=device_name)
-    )
-    match = topic_regex.match(message.topic)
-    if not match:
-        return
+    topic_regex = re.compile(r"{device_name}/lights/(?P<number>\d+)/status/set".format(device_name=device_name))
+    topic_filter = f"{device_name}/lights/+/status/set"
 
-    device_number = int(match.group("number"))
-    op = match.group("op")
-    previous = _CACHE.get(device_number)
-    if not previous:
-        _CACHE[device_number] = previous = 0
+    await mqtt_client.subscribe(topic_filter)
+    async with mqtt_client.filtered_messages(topic_filter) as messages:
+        async for message in messages:
+            logger.info(f"Incoming light command message: {message.payload}")
 
-    if op == _COMMAND_OP:
-        if message.payload == _ON_PAYLOAD and previous == 0:
-            driver.send(DAPC(device_number, 254))
-            await asyncio.gather(
-                mqtt_client.publish(
-                    _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_COMMAND_OP),
-                    _ON_PAYLOAD,
-                ),
-                mqtt_client.publish(
-                    _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_BRIGHTNESS_OP), b"254"
-                ),
+            match = topic_regex.match(message.topic)
+            if not match:
+                logger.warning(f"{message.topic} did not match regex {topic_regex}")
+                continue
+            device_number = int(match.group("number"))
+
+            previous = _CACHE.get(device_number)
+            if not previous:
+                async with _CACHE_LOCK:
+                    response = driver.send(QueryActualLevel(Short(device_number)))
+                    _CACHE[device_number] = previous = response.value
+
+            if message.payload == _ON_PAYLOAD and previous == 0:
+                driver.send(DAPC(Short(device_number), 254))
+                await asyncio.gather(
+                    mqtt_client.publish(
+                        _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_COMMAND_OP),
+                        _ON_PAYLOAD,
+                    ),
+                    mqtt_client.publish(
+                        _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_BRIGHTNESS_OP), b"254"
+                    ),
+                )
+                async with _CACHE_LOCK:
+                    _CACHE[device_number] = 254
+            elif message.payload == _OFF_PAYLOAD and previous != 0:
+                driver.send(Off(Short(device_number)))
+                await asyncio.gather(
+                    mqtt_client.publish(
+                        _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_COMMAND_OP), _OFF_PAYLOAD
+                    ),
+                    mqtt_client.publish(
+                        _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_BRIGHTNESS_OP), b"0"
+                    ),
+                )
+                async with _CACHE_LOCK:
+                    _CACHE[device_number] = 0
+
+
+async def light_brightness_messages(
+    driver: dali.driver.base.SyncDALIDriver,
+    mqtt_client: asyncio_mqtt.Client,
+    device_name: str,
+) -> None:
+
+    topic_regex = re.compile(r"{device_name}/lights/(?P<number>\d+)/brightness/set".format(device_name=device_name))
+    topic_filter = f"{device_name}/lights/+/brightness/set"
+
+    await mqtt_client.subscribe(topic_filter)
+    async with mqtt_client.filtered_messages(topic_filter) as messages:
+        async for message in messages:
+            logger.info(f"Incoming light brightness message: {message.payload}")
+
+            match = topic_regex.match(message.topic)
+            if not match:
+                logger.warning(f"{message.topic} did not match regex {topic_regex}")
+                continue
+            device_number = int(match.group("number"))
+
+            driver.send(DAPC(Short(device_number), int(message.payload)))
+            await mqtt_client.publish(
+                _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_BRIGHTNESS_OP), message.payload
             )
-            _CACHE[device_number] = 254
-        elif message.payload == _OFF_PAYLOAD and previous != 0:
-            driver.send(Off(device_number))
-            await asyncio.gather(
-                mqtt_client.publish(
-                    _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_COMMAND_OP), _OFF_PAYLOAD
-                ),
-                mqtt_client.publish(
-                    _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_BRIGHTNESS_OP), b"0"
-                ),
-            )
-            _CACHE[device_number] = 0
-        else:
-            return
-    # home assistant seems to send and "ON" for each brightness update
-    elif op == _BRIGHTNESS_OP:
-        driver.send(DAPC(device_number, int(message.payload)))
-        await mqtt_client.publish(
-            _TOPIC_FMT.format(device_name=device_name, number=device_number, op=_BRIGHTNESS_OP), message.payload
-        )
-        _CACHE[device_number] = int(message.payload)
-        return
+            async with _CACHE_LOCK:
+                _CACHE[device_number] = int(message.payload)
 
 
 async def amain(mqtt_broker: str, bus_number: int, device_name: str) -> None:
@@ -93,15 +117,12 @@ async def amain(mqtt_broker: str, bus_number: int, device_name: str) -> None:
     driver = dali.driver.unipi.SyncUnipiDALIDriver(bus=bus_number)
     logger.info("Finished setup DALI bus.")
 
-    topic_filter = f"{device_name}/lights/#"
-
     logger.info("Set up MQTT connection.")
     async with asyncio_mqtt.Client(mqtt_broker) as client:
-        logger.info(f"Subscribing to base topic filter {topic_filter}")
-        await client.subscribe(topic_filter)
-        async with client.filtered_messages(topic_filter) as messages:
-            async for message in messages:
-                await process_message(message, driver, client, device_name)
+        await asyncio.gather(
+            light_command_messages(driver, client, device_name),
+            light_brightness_messages(driver, client, device_name),
+        )
 
 
 def main() -> None:
